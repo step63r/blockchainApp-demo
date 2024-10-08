163c13c2cc6eb4bef8c26e098af9c716faf3c758
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2024-10-08 23:44:19 +0900

    up to chapter 3.18

---
+++ contracts/MyERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// OpenZeppelinのERC-20をインポート
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// インポートしたERC-20を継承してMyERC20を作成する
+contract MyERC20 is ERC20 {
+    // トークンの名前を単位を返す
+    constructor() ERC20("MyERC20", "ME2") {
+        // トークンを作成者に1000000渡す
+        _mint(msg.sender, 1000000);
+    }
+}
--- scripts/deploy-local.ts
+++ scripts/deploy-local.ts
@@ -4,6 +4,10 @@ async function main() {
   const myToken = await ethers.deployContract("MyToken");
   myToken.waitForDeployment();
   console.log(`MyToken deployed to: ${myToken.target}`);
+
+  const myERC20 = await ethers.deployContract("MyERC20");
+  myERC20.waitForDeployment();
+  console.log(`MyERC20 deployed to: ${myERC20.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
